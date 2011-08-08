82d4116b0200032e377da46be9f75a8c2497e806
xe.master153 <xe.master153@gmail.com>
2011-08-08 03:34:08 +0000

    git-svn-id: http://xe-contact.googlecode.com/svn/trunk/modules/contact@9 305acff9-b2bc-ff07-1a19-1942a3cd78bc

--- lang/ko.lang.php
+++ lang/ko.lang.php
@@ -1,42 +1,39 @@
 <?php
     /**
-     * @file   en.lang.php
+     * @file   ko.lang.php
      * @author developer (developer@nhn.com)
      * @brief contact modules's basic language pack
      **/
 
     $lang->contact = 'Contact Us';
 
-	$lang->contact_management = 'Contact Us Management';
-	$lang->about_contact = 'This module is for creating and managing Contact Us pages.';
+	$lang->contact_management = 'Contact Us 관리';
+	$lang->about_contact = '이 모듈은 Contact Us 페이지를 생성하거나 관리하는데 사용됩니다.';
 
-	$lang->cmd_contact_list = 'Contact Us Lists';
-	$lang->cmd_create_contact = 'Create a new Contact Us';
-    $lang->cmd_view_info = 'Contact Us Info';
-	$lang->cmd_form_components = 'Form Extra Components';
-	$lang->cmd_agreement = 'Agreement Term';
-	$lang->form_comps = 'Form Extra Components';
-	$lang->eid = 'Name of Form Component';
-	$lang->input_type = 'Input Type';
+	$lang->cmd_contact_list = 'Contact Us 리스트';
+	$lang->cmd_create_contact = 'Contact Us 생성';
+    $lang->cmd_view_info = 'Contact Us 정보';
+	$lang->cmd_form_components = 'form 요소추가';
+	$lang->cmd_agreement = '라이센스';
+	$lang->form_comps = 'form 요소추가';
+	$lang->eid = '컴포넌트 ID';
+	$lang->input_type = '컴포넌트 종류';
 
-	$lang->input_type_list['text'] = 'One-Line Text';
-	$lang->input_type_list['textarea'] = 'Multi-Line Textarea';
-	$lang->input_type_list['select'] = 'Select Box (single selection)';
-	$lang->input_type_list['checkbox'] = 'Checkbox (multiple selection)';
-	$lang->input_type_list['radio'] = 'Radio Button (radio)';
-	$lang->input_type_list['tel'] = 'Phone Number';
-	$lang->input_type_list['date'] = 'Date (yyyy/mm/dd)';
+	$lang->input_type_list['text'] = '텍스트 (text)';
+	$lang->input_type_list['textarea'] = 'textarea';
+	$lang->input_type_list['select'] = '셀렉트박스(select)';
+	$lang->input_type_list['checkbox'] = '체크박스(checkbox)';
+	$lang->input_type_list['radio'] = '라디오 버튼(radio)';
+	$lang->input_type_list['tel'] = '전화번호';
+	$lang->input_type_list['date'] = '날짜 (yyyy/mm/dd)';
 
-	$lang->enable_terms = 'Enable Term';
-	$lang->about_enable_terms = 'If checked, an agreement term will be enabled.';
-	$lang->enable_site_contact = 'Enable Site Contact';
-	$lang->about_enable_site_contact = 'If checked, the site contact information will be enabled.';
-	$lang->cmd_modify_term = 'Modify Term';
-	$lang->admin_mail = 'Administrator\'s Mail';
-	$lang->about_admin_mail	= 'Multiple mails can be sent with commas(,).';
-	$lang->components_count = "Form Components";
+	$lang->enable_terms = '라이센스 표시여부';
+	$lang->about_enable_terms = '체크하시면 화면에 라이센스 내용이 표시됩니다.';
+	$lang->admin_mail = '관리자 e-mail';
+	$lang->about_admin_mail	= '여러개의 관리자 e-mail을 사용할려면 콤마(,)를 추가하시면 됩니다.ex:aaa@aa.com,bb@bb.com';
+	$lang->components_count = "form에 포함된 컴포넌트 수";
 
-	$lang->interval = 'Interval for spam filtering';
-	$lang->about_interval = 'All mails within the assigned time will be blocked.';
-	$lang->msg_alert_limited_by_config_mail = 'Sending an email within %s minute is not allowed.';
+	$lang->interval = '스펨메일 필터';
+	$lang->about_interval = '설정된 시간 이후에 다음 메일을 보낼 수 있습니다.';
+	$lang->msg_alert_limited_by_config_mail = '%s분 이내 연속으로 메일을 발송 할 수 없습니다.';
 ?>
