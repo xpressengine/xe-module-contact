e89b57c7fadc22b411399a44f9824e4aa578e690
xe.master153 <xe.master153@gmail.com>
2011-08-09 03:17:50 +0000

    git-svn-id: http://xe-contact.googlecode.com/svn/trunk/modules/contact@10 305acff9-b2bc-ff07-1a19-1942a3cd78bc

--- lang/zh-CN.lang.php
+++ lang/zh-CN.lang.php
@@ -1,42 +1,42 @@
 <?php
     /**
-     * @file   en.lang.php
+     * @file   zh-CN.lang.php
      * @author developer (developer@nhn.com)
      * @brief contact modules's basic language pack
      **/
 
     $lang->contact = 'Contact Us';
 
-	$lang->contact_management = 'Contact Us Management';
-	$lang->about_contact = 'This module is for creating and managing Contact Us pages.';
+	$lang->contact_management = 'Contact Us 管理';
+	$lang->about_contact = '此模块用于搭建和管理 Contact Us页面.';
 
-	$lang->cmd_contact_list = 'Contact Us Lists';
-	$lang->cmd_create_contact = 'Create a new Contact Us';
-    $lang->cmd_view_info = 'Contact Us Info';
-	$lang->cmd_form_components = 'Form Extra Components';
-	$lang->cmd_agreement = 'Agreement Term';
-	$lang->form_comps = 'Form Extra Components';
-	$lang->eid = 'Name of Form Component';
-	$lang->input_type = 'Input Type';
+	$lang->cmd_contact_list = 'Contact Us 列表';
+	$lang->cmd_create_contact = '新建 Contact Us';
+    $lang->cmd_view_info = 'Contact Us 信息';
+	$lang->cmd_form_components = '表单附加组件';
+	$lang->cmd_agreement = '协议条款';
+	$lang->form_comps = '表单附加组件';
+	$lang->eid = '组件名称';
+	$lang->input_type = '组件类别';
 
-	$lang->input_type_list['text'] = 'One-Line Text';
-	$lang->input_type_list['textarea'] = 'Multi-Line Textarea';
-	$lang->input_type_list['select'] = 'Select Box (single selection)';
-	$lang->input_type_list['checkbox'] = 'Checkbox (multiple selection)';
-	$lang->input_type_list['radio'] = 'Radio Button (radio)';
-	$lang->input_type_list['tel'] = 'Phone Number';
-	$lang->input_type_list['date'] = 'Date (yyyy/mm/dd)';
+	$lang->input_type_list['text'] = '单行文本框';
+	$lang->input_type_list['textarea'] = '多行文本框';
+	$lang->input_type_list['select'] = '下拉列表框 (单选)';
+	$lang->input_type_list['checkbox'] = '复选框 (多选)';
+	$lang->input_type_list['radio'] = '单选框 (单选)';
+	$lang->input_type_list['tel'] = '电话号码';
+	$lang->input_type_list['date'] = '日期 (yyyy/mm/dd)';
 
-	$lang->enable_terms = 'Enable Term';
-	$lang->about_enable_terms = 'If checked, an agreement term will be enabled.';
-	$lang->enable_site_contact = 'Enable Site Contact';
-	$lang->about_enable_site_contact = 'If checked, the site contact information will be enabled.';
-	$lang->cmd_modify_term = 'Modify Term';
-	$lang->admin_mail = 'Administrator\'s Mail';
-	$lang->about_admin_mail	= 'Multiple mails can be sent with commas(,).';
-	$lang->components_count = "Form Components";
+	$lang->enable_terms = '使用协议条款';
+	$lang->about_enable_terms = '在页面中使用协议条款.';
+	$lang->enable_site_contact = '使用联系信息';
+	$lang->about_enable_site_contact = '在页面中显示联系信息.';
+	$lang->cmd_modify_term = '修改协议';
+	$lang->admin_mail = '管理者邮箱';
+	$lang->about_admin_mail	= '多个邮箱地址可用逗号（，）分开.';
+	$lang->components_count = "表单组件";
 
-	$lang->interval = 'Interval for spam filtering';
-	$lang->about_interval = 'All mails within the assigned time will be blocked.';
-	$lang->msg_alert_limited_by_config_mail = 'Sending an email within %s minute is not allowed.';
+	$lang->interval = '垃圾邮件过滤';
+	$lang->about_interval = '所有邮件在指定的时间内都被过滤.';
+	$lang->msg_alert_limited_by_config_mail = '禁止在 %s 分钟内连续发送 邮件.';
 ?>
