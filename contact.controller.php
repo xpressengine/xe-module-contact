90d44301cfaa0026698e041ebd33c9a14d7adada
ChanMyeong <dece24@gmail.com>
2013-03-20 01:28:21 +0000

    XE 1.7 admin UI optimization. Copy files.
    git-svn-id: http://xe-contact.googlecode.com/svn/branches/1.7.0@38 305acff9-b2bc-ff07-1a19-1942a3cd78bc

---
+++ contact.controller.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * @class  contactController
+ * @author NHN (developers@xpressengine.com)
+ * @brief  contact module Controller class
+ **/
+
+class contactController extends contact {
+
+	/**
+	 * @brief initialization
+	 **/
+	function init() {
+	}
+
+	/**
+	 * @brief send email 
+	 **/
+	function procContactSendEmail(){
+		$logged_info = Context::get('logged_info');
+		if($this->module_info->send_grant_all != 'Y' && !$logged_info)
+			return new Object(-1, 'Only logged user can send an email.');
+
+		$oMail = new Mail();
+
+		$oMail->setContentType("plain");
+
+		// get form variables submitted
+		$obj = Context::getRequestVars();
+		if($obj->enable_terms == 'Y' && !$obj->check_agree) return new Object(-1, 'You haven\'t read and agree to the terms of the license agreement.');
+
+		$obj->email = $obj->Email;
+		$obj->subject = $obj->Subject;
+		$obj->comment = $obj->Comment;
+		
+		$oDocumentModel = &getModel('document');
+		$extra_keys = $oDocumentModel->getExtraKeys($obj->module_srl);
+
+		$mail_content = array();
+
+		$content = '';
+		if(count($extra_keys)) {
+			foreach($extra_keys as $idx => $extra_item) {
+				$value = '';
+				if(isset($obj->{'extra_vars'.$idx})) $value = $obj->{'extra_vars'.$idx};
+				elseif(isset($obj->{$extra_item->eid})) $value = $obj->{$extra_item->eid};
+				if(!is_array($value)) $value = trim($value);
+				if(!isset($value)) continue;
+				//check if extra item is required
+				if($extra_item->is_required == 'Y' && $value==""){
+					return new Object(-1, 'Please input a value for '.$extra_item->name);
+				}
+				//if the type of form component is email address
+				if($extra_item->type == 'email_address' && !$oMail->isVaildMailAddress($value)){
+					return new Object(-1, 'Please input a valid email for '.$extra_item->name);
+				}
+				if($extra_item->type == "tel")
+				{
+					$mail_content[$extra_item->eid] = $obj->{'extra_vars'.$idx}[2];
+					$content .= $extra_item->eid. ':  ' . $obj->{'extra_vars'.$idx}[2] . "\r\n";
+				}
+				elseif(is_array($obj->{'extra_vars'.$idx}))
+				{
+					$mail_content[$extra_item->eid] = implode(",",$obj->{'extra_vars'.$idx});
+					$content .= $extra_item->eid. ':  ' . implode(",",$obj->{'extra_vars'.$idx}) . "\r\n";
+				}
+				else
+				{
+					$mail_content[$extra_item->eid] = $value;
+					$content .= $extra_item->eid. ':  ' . $value . "\r\n";
+				}
+			}
+		}
+
+		//if the admin mail is not set, then admin mail equals to admin registered email address
+		if(!count($this->module_info->admin_mail)>0) {
+			$this->module_info->admin_mail = $logged_info->email_address;
+		}
+		
+		if(!$oMail->isVaildMailAddress($obj->email)){
+			return new Object(-1, 'Please input your valid email address.');
+		}
+
+		$oMail->setTitle($obj->subject);
+		$content_all = $content . "\r\nComments:\r\n" . htmlspecialchars($obj->comment);
+		$mail_content['Comments'] = htmlspecialchars($obj->comment);
+
+		$oMail->setContent(htmlspecialchars($content_all));
+		//$oMail->setSender("XE Contact Us", $obj->email);
+		$oMail->setSender($obj->email, $obj->email);
+
+		$target_mail = explode(',',$this->module_info->admin_mail);
+
+		for($i=0;$i<count($target_mail);$i++) {
+			$email_address = trim($target_mail[$i]);
+			if(!$email_address) continue;
+			if(!$oMail->isVaildMailAddress($email_address)) $email_address = $logged_info->email_address;
+			$oMail->setReceiptor($email_address, $email_address);
+
+			if($logged_info->is_admin != 'Y'){
+				if($this->module_info->module_srl){
+					$oModuleModel = &getModel('module');
+					$moduleExtraVars = $oModuleModel->getModuleExtraVars($this->module_info->module_srl);
+					if($moduleExtraVars[$this->module_info->module_srl]->interval){
+						$interval = $moduleExtraVars[$this->module_info->module_srl]->interval;
+						//transfer interval to mins
+						$interval = $interval*60;
+						$oContactModel = &getModel('contact');
+						$output = $oContactModel->checkLimited($interval);	
+						if(!$output->toBool()) return $output;
+					}
+				}
+			}
+			$oMail->send();
+		}
+
+
+		if(isset($_SESSION['mail_content']))
+			unset($_SESSION['mail_content']);
+
+		$_SESSION['mail_content'] = $mail_content;
+
+
+		if($logged_info->is_admin != 'Y'){
+			$oSpamController = &getController('spamfilter');
+			$oSpamController->insertLog();
+		}
+
+		$msg_code = 'An email has been sent successfully.';
+		$this->add('mid', Context::get('mid'));
+
+		$this->setMessage($msg_code);
+
+		if(!in_array(Context::getRequestMethod(),array('XMLRPC','JSON'))) {
+			$returnUrl = Context::get('success_return_url') ? Context::get('success_return_url') : getNotEncodedUrl('', 'act', 'dispCompleteSendMail','mid', $obj->mid);
+			header('location:'.$returnUrl);
+			return;
+		}
+	}
+
+}
+?>
---
+++ contact.view.php
@@ -0,0 +1,104 @@
+<?php
+    /**
+     * @class  ContactView
+     * @author NHN (developers@xpressengine.com)
+     * @brief  contact us module View class
+     **/
+
+    class contactView extends contact {
+
+
+        /**
+         * @brief initialize contact view class.
+         **/
+		function init() {
+           /**
+             * get skin template_path
+             * if it is not found, default skin is xe_contact
+             **/
+            $template_path = sprintf("%sskins/%s/",$this->module_path, $this->module_info->skin);
+            if(!is_dir($template_path)||!$this->module_info->skin) {
+                $this->module_info->skin = 'xe_contact_official';
+                $template_path = sprintf("%sskins/%s/",$this->module_path, $this->module_info->skin);
+            }
+            $this->setTemplatePath($template_path);
+
+            /**
+             * get extra variables from xe_document_extra_keys table, context set
+             **/
+            $oDocumentModel = &getModel('document');
+            $form_extra_keys = $oDocumentModel->getExtraKeys($this->module_info->module_srl);
+            Context::set('form_extra_keys', $form_extra_keys);
+		}
+
+        /**
+         * @brief display contact content
+         **/
+        function dispContactContent() {
+
+			Context::addJsFilter($this->module_path.'tpl/filter', 'search.xml');
+			Context::addJsFilter($this->module_path.'tpl/filter', 'send_email.xml');
+
+			// set template_file to be list.html
+            $this->setTemplateFile('index');
+        }
+
+
+        /**
+         * @brief display agreement term write form
+         **/
+        function dispContactTermWrite() {
+
+			// only admin user can write contact term
+			if(!Context::get('is_logged'))  return $this->setTemplateFile('input_password_form');
+            $logged_info = Context::get('logged_info');
+            if($logged_info->is_admin != 'Y') return $this->setTemplateFile('input_password_form');
+
+			$oContactModel = &getModel('contact');
+			$editor_content = $oContactModel->getEditor($this->module_info->module_srl);
+			Context::set('editor_content', $editor_content);
+
+            /** 
+             * add javascript filter file insert_question
+             **/
+			$termText = $this->getTermText();
+			Context::set('termText', $termText);
+
+            Context::addJsFilter($this->module_path.'tpl/filter', 'insert_term.xml');
+
+            $this->setTemplateFile('write_term_form');
+
+		}
+
+		function dispCompleteSendMail() {
+
+			if(isset($_SESSION['mail_content'])){
+				$mail_content = $_SESSION['mail_content'];
+				Context::set('mail_content',$mail_content);
+
+			}else{
+				Context::set('mail_content','');
+				$url = getUrl('mid',$this->mid,'act','');
+				header('Location: '.$url);
+			}
+
+			unset($_SESSION['mail_content']);
+
+			$this->setTemplateFile('success_form');
+		}
+
+		function getTermText($strlen = 0) {
+            if(!$this->module_info->module_srl) return;
+
+			$term = $this->module_info->content;
+
+			if($strlen) return cut_str(strip_tags($term),$strlen,'...');
+
+			return htmlspecialchars($term);
+		}
+
+    }
+
+
+
+?>
